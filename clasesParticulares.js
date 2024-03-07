4ccdd23b796693b2369f301850865a7e2bbb7588
ValentinDaSilva <valedasilvacatela.vdc@gmail.com>
2024-03-07 13:36:02 -0300

    auto-commit

--- clasesParticulares.js
+++ clasesParticulares.js
@@ -1,3 +1,13 @@
+let $opcion1 = document.getElementById("opcion1");
+let $opcion2 = document.getElementById("opcion2");
+let $opciones = document.querySelectorAll(".opciones input");
+let $imagen1 = document.getElementById("imagen1");
+let $imagen2 = document.getElementById("imagen2");
+let $oficinasTitulo = document.querySelector(".oficinas .encabezado");
+let $oficinasGaleriaTexto = document.querySelector(".oficinas .GaleriaTexto");
+$oficinasTitulo.style.opacity = "0";
+$oficinasGaleriaTexto.style.opacity = "0";
+
 document.addEventListener("DOMContentLoaded", function() {
   // Aquí puedes escribir el código que deseas ejecutar cuando el DOM haya sido completamente cargado
   let $mensajeInicio = document.querySelector(".mensajeInicio h2");
@@ -35,40 +45,55 @@ const verificarVisibilidad = (entradas) => {
         })
         }
       // Verificar si el elemento 2 es intersectado
-      if (entrada.isIntersecting && entrada.target.id === "seccion2") {
+      else if (entrada.isIntersecting && entrada.target.id === "seccion2") {
         $hijosClases2.forEach((elem) => {
             elem.classList.add("animate__animated");
             elem.classList.add("animate__fadeIn");
         })
       }
-    });
-    entradas.forEach(entrada => {
       // Verificar si el elemento 1 es intersectado
-      if (!entrada.isIntersecting && entrada.target.id === "seccion1") {
+      else if (!entrada.isIntersecting && entrada.target.id === "seccion1") {
         $hijosClases1.forEach((elem) => {
             elem.classList.remove("animate__animated");
             elem.classList.remove("animate__fadeIn");
         })
         }
       // Verificar si el elemento 2 es intersectado
-      if (!entrada.isIntersecting && entrada.target.id === "seccion2") {
+      else if (!entrada.isIntersecting && entrada.target.id === "seccion2") {
         $hijosClases2.forEach((elem) => {
             elem.classList.remove("animate__animated");
             elem.classList.remove("animate__fadeIn");
         })
       }
+      else if (entrada.isIntersecting && entrada.target.classList.contains("encabezado")) {
+        entrada.target.classList.add("animate__animated");
+        entrada.target.classList.add("animate__fadeIn");
+      }
+      // Verificar si el elemento 2 es intersectado
+      else if (!entrada.isIntersecting && entrada.target.classList.contains("encabezado")) {
+            entrada.target.classList.remove("animate__animated");
+            entrada.target.classList.remove("animate__fadeIn");
+      }
+      else if (entrada.isIntersecting && entrada.target.classList.contains("GaleriaTexto")) {
+        entrada.target.classList.add("animate__animated");
+        entrada.target.classList.add("animate__fadeIn");
+      }
+      // Verificar si el elemento 2 es intersectado
+      else if (!entrada.isIntersecting && entrada.target.classList.contains("GaleriaTexto")) {
+            entrada.target.classList.remove("animate__animated");
+            entrada.target.classList.remove("animate__fadeIn");
+      }
+      
+
     });
   };
 
 const observer = new IntersectionObserver(verificarVisibilidad,{threshold: 0.5});
 observer.observe($clases1);
 observer.observe($clases2);
+observer.observe($oficinasTitulo);
+observer.observe($oficinasGaleriaTexto);
 
-let $opcion1 = document.getElementById("opcion1");
-let $opcion2 = document.getElementById("opcion2");
-let $opciones = document.querySelectorAll(".opciones input");
-let $imagen1 = document.getElementById("imagen1");
-let $imagen2 = document.getElementById("imagen2");
 
 $opciones.forEach((elem)=>{
   elem.addEventListener("click",()=>{
