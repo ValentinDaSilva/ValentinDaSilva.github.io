87ded335b988a9359189018052ef4ddf365fcde0
Valentin <67626546+ValentinDaSilva@users.noreply.github.com>
2024-05-19 10:03:09 -0300

    Update IniciarSesion.js

--- IniciarSesion/IniciarSesion.js
+++ IniciarSesion/IniciarSesion.js
@@ -29,11 +29,7 @@ $botonEnviar.addEventListener("click",(e)=>{
         formData.forEach((value, key) => {
             formContent += `${key}: ${value}\n`;
         });
-    let valores = formData.entries();
     let datosAEnviar = [];
-    valores.forEach((element, indice) => {
-      datosAEnviar.push(element[1]);
-    });
     datosAEnviar[0] = datosAEnviar[0].toLowerCase();
     console.log(datosAEnviar);
     let urlFinal = url + "?correo=" + datosAEnviar[0] + "&contrasenia=" + datosAEnviar[1];
